d4803caa0f30b46bb1564ea63aac73d64c254a2f
Moamin <111950883+AmooniG@users.noreply.github.com>
2023-09-26 00:28:07 -0400

    word_tally.c

--- A1-main/word_tally.c
+++ A1-main/word_tally.c
@@ -1,92 +1,76 @@
 #include <stdio.h>
-#include <stdlib.h> // for malloc(), free()
+#include <stdlib.h>
 #include <string.h>
-
+#include "LLNode.h"
 #include "word_extractor.h"
-#include "word_tally.h"
 
-/**
- * A tool to hold the words we care about in an array, and
- * provide a method for processing the file.
- *
- */
+int printData(char *filename, LLNode **wordListHeads, int maxLen);
+int printHapax(char *filename, LLNode **wordListHeads, int maxLen, int hapaxLength);
 
-// forward declarations
-static int updateWordInTallyList(LLNode **wordLists, int maxLen, char *word);
+// Function to tally words in a file
+int tallyWordsInFile(char *filename, LLNode **wordLists, int maxLen) {
+	//creating a word extractor for a specific given file
+    struct WordExtractor *we = weCreateExtractor(filename, maxLen);
+    if (!we) {
+        fprintf(stderr, "Error: Could not open file %s\n", filename);
+        return -1;
+    }
+	int wordCount = 0; // setting word count to 0
 
-/*
- * Here we do all the work, processing the
- * file and determining what to do for each word as we
- * read it.  Once we are done reading, we can then
- * examine our wordsToCheck list and see what their
- * state is
- */
-int tallyWordsInFile(char *filename, LLNode **wordLists, int maxLen)
-{
-	struct WordExtractor *wordExtractor = NULL;
-	char *aWord = NULL;
-	// add any more variables that you need
+	//while loop to go through each word in the file
+    while (weHasMoreWords(we)) {
+        char *word = weGetNextWord(we);
+        int len = strlen(word);
+        if (len > maxLen) {
+            continue;  // if length of a word is over the maximum it skips it
+        }
 
-
-	// create the extractor and open the file
-	wordExtractor = weCreateExtractor(filename, maxLen);
-
-	if (wordExtractor == NULL) {
-		fprintf(stderr, "Failed creating extractor for '%s'\n", filename);
-		return 0;
-	}
-
-	/** TODO: ensure that all of the word list heads are NULL */
-
-
-	// read each word from the file using the WordExtractor,
-	// and for each tally how often it has been used in
-	// the appropriate list
-
-	// In the loop, figure out whether we have seen this
-	// word before, and do the appropriate thing if we have
-
-	int totalWordCount = 0;
-
-	while (weHasMoreWords(wordExtractor)) {
-		aWord = weGetNextWord(wordExtractor);
-		totalWordCount++;
-
-		// figure out whether we have seen this word before,
-		// and do the appropriate thing if we have
-		updateWordInTallyList(wordLists, maxLen, aWord);
-	}
-
-	// is there anything else you need to do at the end of the loop?
-	printf("Total word count %d\n", totalWordCount);
-
-	// Close the file when we are done
-	weDeleteExtractor(wordExtractor);
-
-	return 1;
+		//checks if word already exists
+        LLNode *node = llLookupKey(wordLists[len], word);
+        if (node) {
+			// Increment count for existing word
+            node->value++;  
+        } else {
+            // Add new word to the list with count 1
+            node = llNewNode(strdup(word), 1);
+            wordLists[len] = llAppend(wordLists[len], node);
+        }
+		wordCount++;
+    }
+	//cleanup (deletes the extractor)
+    weDeleteExtractor(we);
+    return wordCount;
 }
 
-/**
- * TODO: Either update the tally in the list, or add it to the list
- */
-static int
-updateWordInTallyList(LLNode **wordListHeads, int maxLen, char *word)
-{
-
-	/* TODO: look up the word in the correct list to see
-	 * if we have already seen it */
-
-
-
-	/* TODO: if the word is in the list, then update the tally
-	 * in the node and we are done so return success */
-	
-
-
-	/** TODO: otherwise add it to the list */
-
-
-	/** return success if no error */
-	return 1;
+// Function to print all data loaded
+int printData(char *filename, LLNode **wordListHeads, int maxLen) {
+    printf("Data from file '%s':\n", filename);
+	// a for loop to go through words of all lengths up to maximum length
+    for (int i = 0; i <= maxLen; i++) {
+        LLNode *current = wordListHeads[i];
+        while (current) {
+            printf("%s: %d\n", current->key, current->value);
+            current = current->next;
+        }
+    }
+    return 0;
 }
 
+// Function to print hapax legomena
+int printHapax(char *filename, LLNode **wordListHeads, int maxLen, int hapaxLength) {
+    printf("Hapax legomena from file '%s':\n", filename);
+	// a for loop to go through words of all lengths up to maximum length
+    for (int i = 0; i <= maxLen; i++) {
+        if (hapaxLength == 0 || hapaxLength == i) {
+            LLNode *current = wordListHeads[i];
+            while (current) {
+				//if words appear only once its a hapax legomenon
+                if (current->value == 1) {
+                    printf("%s\n", current->key);
+                }
+                current = current->next; //move to next node
+            }
+        }
+    }
+    return 0;
+}
