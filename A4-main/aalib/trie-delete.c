77cb462201daaa865b4d76cce0dfed189cc07e0b
Moamin <111950883+AmooniG@users.noreply.github.com>
2023-11-29 23:40:13 -0500

    Update trie-delete.c

--- A4-main/aalib/trie-delete.c
+++ A4-main/aalib/trie-delete.c
@@ -17,7 +17,31 @@ walk_chain_to_delete(
 	)
 {
 	// TO DO: remove nodes for deleted key
-	return 0;
+    if (keylength == 0 || curSearchNode == NULL) {
+        return -1; // Key not found or end of chain
+    }
+
+    if (curSearchNode->letter == key[0]) {
+        if (keylength == 1) {
+            // Last letter of the key, remove the value
+            *value = curSearchNode->value;
+            curSearchNode->value = NULL;
+            if (cost) (*cost)++;
+            return 0; // Key found and value removed
+        } else {
+            // Continue searching down the chain
+            return walk_chain_to_delete(value, curSearchNode->subtries[0], key + 1, keylength - 1, cost);
+        }
+    } else {
+        // Continue searching in the next subtrie
+        for (int i = 0; i < curSearchNode->nSubtries; i++) {
+            if (walk_chain_to_delete(value, curSearchNode->subtries[i], key, keylength, cost) == 0) {
+                return 0; // Key found and value removed in a subtrie
+            }
+        }
+    }
+
+    return -1; // Key not found in this path
 }
 
 /** delete a key from the trie */
@@ -37,6 +61,11 @@ void *trieDeleteKey(
 	/**
 	 ** TO DO: search for the right subchain and delete the key from it
 	 **/
+	    for (int i = 0; i < root->nSubtries; i++) {
+        if (walk_chain_to_delete(&valueFromDeletedKey, root->subtries[i], key, keylength, cost) == 0) {
+            return valueFromDeletedKey; // Key found and value removed
+        }
+    }
 	
 	return valueFromDeletedKey;
 }
